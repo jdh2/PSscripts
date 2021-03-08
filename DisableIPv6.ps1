4d360d657f747648c8108812697f7eb8a544de78
jdh2 <howe.jd@live.com>
2021-03-08 16:15:33 -0600

    Add files via upload

---
+++ DisableIPv6.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "SilentlyContinue"
+$logfolder = "$env:programdata\DisableIPv6\"
+$logfile = "$logfolder\DisableIPv6.log"
+$regvalue = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\DisableComponents"
+
+#Check if the logfolder directory exists, if not create it
+if (!(Test-Path -Path $logfolder)) {
+    New-Item -Path $logfolder -ItemType Directory -Force
+}
+
+#Check if the logfile already exists and execute code if it does not
+if (!(Test-Path -Path $logfile)) {
+
+    #Check if the DisabledComponents registry value exists, if it does not, create it and create a text file
+    if (!(Test-Path -path $regvalue)) {
+        New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -Name DisabledComponents -Value 255 -PropertyType DWord
+        New-Item -ItemType File -Path "$logfolder\DisableIPv6.txt"
+        
+    }
+
+}
